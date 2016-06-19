62d6aa38db8623146a47540ef714a0c1b5b385a5
BadAssSocialTime <Cary>
2016-06-19 16:53:04 -0400

    Committed some fundamental stuff of Scala

--- src/FirstScalaClass.scala
+++ src/FirstScalaClass.scala
@@ -1,8 +1,48 @@
 /**
-  * Created by mr_maggie on 6/18/16.
+  * Created by Cary Ma on 6/18/16.
   */
 class FirstScalaClass {
   def SayHelloWorld: Unit ={
     println("Hello World!")
   }
+
+  val mustang = new Car("Ford", "Mustang", "Red")
+  val corvette = new Car("GM", "Corvette", "Black")
+  //case class without new
+  val request = Message("harry", "sam", "fight")
+  //Pattern matching
+  def f(x: Int, y: Int, operator: String): Double = {
+    operator match {
+      case "+" => x + y
+      case "-" => x - y
+      case "*" => x * y
+      case "/" => x / y.toDouble
+    }
+  }
+  val sum = f(10,20, "+")
+  val product = f(10, 20, "*")
+
+  def colorToNumber(color: String): Int = {
+    val num = color match {
+    case "Red" => 1
+    case "Blue" => 2
+    case "Green" => 3
+    case "Yellow" => 4
+    case _ => 0
+    }
+    num
+  }
+
 }
+
+class Car(mk: String, ml: String, cr: String) {
+  val make = mk
+  val model = ml
+  var color = cr
+  def repaint(newColor: String) = {
+    color = newColor
+  }
+}
+//case class
+case class Message(from: String, to: String, content: String)
+
--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -1,5 +1,32 @@
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("Hello World!")
+    //Function Literals
+    val code = encode(10, (x: Int) => x + 100)
+
+
+    println(code)
   }
+  //Higher-Order Methods
+  def encode(n: Int, f: (Int) => Long): Long = {
+    val x = n * 10
+    f(x)
+  }
+  //Closures
+  def encodeWithSeed(num: Int, seed: Int): Long = {
+    def encode(x: Int, func: (Int) => Int): Long = {
+      val y = x + 1000
+      func(y)
+    }
+    val result = encode(num, (n: Int) => n * seed)
+    result
+  }
+}
+
+//Object is keyword for singleton class4
+object DatabaseConnection {
+  def open(name: String): Int = {
+     1
+  }
+  def close (): Unit = {}
 }
